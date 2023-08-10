65fd98f2ddcc624a0a4de7ab063277f7432de731
Kamal0078k <saikamal0078@gmai.com>
2023-08-10 18:32:14 +0530

    UI

--- src/Components/Receive.jsx
+++ src/Components/Receive.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useRef } from "react";
 import QRCode from "react-qr-code";
+import circle1 from "./../assets/circle1.svg";
 import Peer from "peerjs";
+import circ from "./../assets/sendcirc.svg";
 import receiver from "./../assets/receiver.png";
 
 const Receive = () => {
@@ -71,11 +73,20 @@ const Receive = () => {
   };
 
   return (
-    <div className="bg-[#eae8f2] h-screen flex flex-col items-center justify-center ">
-      <div className="flex flex-col items-center justify-center py-10 rounded-xl px-5 bg-[#ffffff]">
+    <div className="h-screen w-screen flex flex-col justify-center items-center relative">
+      <img
+        src={circle1}
+        alt=""
+        className="absolute top-[60%] left-[70%] md:top-[56%] h-[5rem] md:h-[10rem]  md:left-[54%] blur-lg opacity-60"
+      />
+      <img
+        src={circ}
+        className="sm:h-[20rem] opacity-60 h-[10rem]  absolute top-[25%] sm:top-[10%] right-[58%] sm:right-[53%] blur-lg"
+      />
+      <div className="absolute z-10 flex flex-col gap-4 justify-center items-center bg-gradient-to-br px-10 py-10 from-[rgba(255,255,255,0.2)] to-[rgba(0,0,0,0.03)]  rounded-2xl backdrop-blur-xl ">
         {receiverId && (
           <QRCode
-            title="GeeksForGeeks"
+            title="IDQR"
             value={receiverId}
             bgColor={"#FFFFFF"}
             fgColor={"#000000"}
@@ -91,7 +102,6 @@ const Receive = () => {
           receivedName.map((e) => {
             return <div>{e}</div>;
           })}
-
         {receivedFile.length > 0 && (
           <button
             className="bg-[#f4f4f4] w-52  shadow-md rounded-xl hover:bg-[#fc6b68] mt-5  hover:text-[#ffffff] hover:shadow-xl text-2xl py-2"
@@ -102,6 +112,37 @@ const Receive = () => {
         )}
       </div>
     </div>
+    // <div className="bg-[#eae8f2] h-screen flex flex-col items-center justify-center ">
+    //   <div className="flex flex-col items-center justify-center py-10 rounded-xl px-5 bg-[#ffffff]">
+    // {receiverId && (
+    //   <QRCode
+    //     title="GeeksForGeeks"
+    //     value={receiverId}
+    //     bgColor={"#FFFFFF"}
+    //     fgColor={"#000000"}
+    //     size={156}
+    //   />
+    // )}
+    // <p className="font-sans mt-2 text-xs">{receiverId}</p>
+    // {receiving && <div>Receiving....{progress}%</div>}
+    // {receivedFile.length > 0 && (
+    //   <div className="font-sans mt-2">Files Received :</div>
+    // )}
+    // {receivedName.length > 0 &&
+    //   receivedName.map((e) => {
+    //     return <div>{e}</div>;
+    //   })}
+
+    // {receivedFile.length > 0 && (
+    //   <button
+    //     className="bg-[#f4f4f4] w-52  shadow-md rounded-xl hover:bg-[#fc6b68] mt-5  hover:text-[#ffffff] hover:shadow-xl text-2xl py-2"
+    //     onClick={downloadFile}
+    //   >
+    //     Download Files
+    //   </button>
+    // )}
+    //   </div>
+    // </div>
   );
 };
 
